1cc691937b54b6a9170405bc3ce421f30273fc35
João Santos <joaomarcelo.ms@gmail.com>
2022-01-05 02:11:17 -0300

    showing version name

--- app/src/main/java/com/example/testegradle/MainActivity.kt
+++ app/src/main/java/com/example/testegradle/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.testegradle
 
+import android.content.pm.PackageInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import showToast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val pInfo: PackageInfo =
+            this.applicationContext.packageManager
+                .getPackageInfo(this.applicationContext.packageName, 0)
+
+        showToast(pInfo.versionName)
+
     }
 }
