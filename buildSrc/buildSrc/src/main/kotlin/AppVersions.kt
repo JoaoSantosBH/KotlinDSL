ded89d1867ab6dc4d79a3314cd1172643da90a8f
JoaoSantosBH <joaomarcelo.ms@gmail.com>
2022-01-06 14:29:05 -0300

    adding patch to versions

--- buildSrc/buildSrc/src/main/kotlin/AppVersions.kt
+++ buildSrc/buildSrc/src/main/kotlin/AppVersions.kt
@@ -2,8 +2,8 @@ object AppVersions {
 
     private const val majorNumber = 1
     private const val minorNumber = 0
-    private val buildNumber =
-        System.getenv("JENKINS_BUILD_NUMBER")?.toInt() ?: 1
+    private const val patch = 0
+    private const val buildNumber = 1
 
     val versionCode: Int by lazy {
         println("Version code ===>  $buildNumber")
@@ -11,7 +11,7 @@ object AppVersions {
     }
 
     val versionName: String by lazy {
-        val versionName = "$majorNumber.$minorNumber.$buildNumber"
+        val versionName = "$majorNumber.$minorNumber.$patch.$buildNumber"
         println("Version name ===>  $versionName")
         return@lazy versionName
     }
